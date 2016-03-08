f350c263c128a10dfad0653d50fb7986b66a70af
Alison Hedigliranes <lalodbzkin@hotmail.com>
2016-03-08 18:25:41 -0300

    adc click

---
+++ Apollo2/Assets/Scripts/SoundController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using AssemblyCSharp;
+
+public class SoundController : MonoBehaviour {
+
+	public void som(){
+		if (MainModel.temQuel == true){
+			AudioSource audio = gameObject.AddComponent<AudioSource>();
+			audio.clip = Resources.Load ("Alert") as AudioClip;
+			audio.Play();
+		}
+	}
+}
