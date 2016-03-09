088d7cf805b9e396e5cd3ec542ffcb32019d34ce
Thiago Anderson <thiagoanderson16@gmail>
2016-03-08 21:25:51 -0300

    Implementação da tela inicial

---
+++ Apollo2/Assets/Scripts/MenuView.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuView : MonoBehaviour {
+
+	public GameObject menuInformacoes;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void moverPainel(){
+		menuInformacoes.transform.position = new Vector3(0,0,0);
+	}
+		
+}
